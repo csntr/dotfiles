0eea5fc736b5bd95880064698534350d157c5491
Tamas Csontos <tamas@graphaware.com>
2024-02-10 12:26:33 +0100

    add highlighting on yank

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -12,3 +12,14 @@ vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
