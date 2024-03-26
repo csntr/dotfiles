31ad9560449e8fe71ad694e42e8ff95a0394575a
Tamas Csontos <csontos.t77@gmail.com>
2024-03-26 20:40:48 +0100

    nvim: add formatting on save

--- nvim/lua/plugins/code-style.lua
+++ nvim/lua/plugins/code-style.lua
@@ -1,17 +1,33 @@
 return {
-  { "tpope/vim-sleuth" },
-  {
-    "Gnvimtools/none-ls.nvim",
-    config = function()
-      local null_ls = require("null-ls")
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.stylua,
-          null_ls.builtins.formatting.prettier,
-        },
-      })
-
-      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format Buffer" })
-    end,
-  },
+	{ "tpope/vim-sleuth" },
+	{
+		"Gnvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.prettier,
+				},
+				on_attach = function(client, bufnr)
+					-- Enable formatting on sync
+					if client.supports_method("textDocument/formatting") then
+						local format_on_save = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							group = format_on_save,
+							buffer = bufnr,
+							callback = function()
+								vim.lsp.buf.format({
+									bufnr = bufnr,
+									filter = function(_client)
+										return _client.name == "null-ls"
+									end,
+								})
+							end,
+						})
+					end
+				end,
+			})
+		end,
+	},
 }
