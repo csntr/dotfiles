65b11ea3dad7ca9295d04a71c04256f86e3ce805
Tamas Csontos <csontos.t77@gmail.com>
2024-03-26 18:26:24 +0100

    nvim: add vim sleuth for auto indenting

---
+++ nvim/lua/plugins/code-style.lua
@@ -0,0 +1,17 @@
+return {
+  { "tpope/vim-sleuth" },
+  {
+    "Gnvimtools/none-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.prettier,
+        },
+      })
+
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format Buffer" })
+    end,
+  },
+}
