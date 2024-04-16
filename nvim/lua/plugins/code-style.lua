f2d32a2b3772c9b921c254e33df8e52447327734
Tamas Csontos <csontos.t77@gmail.com>
2024-04-16 20:20:24 +0200

    nvim: remove none-ls

--- nvim/lua/plugins/code-style.lua
+++ nvim/lua/plugins/code-style.lua
@@ -1,33 +1,3 @@
 return {
 	{ "tpope/vim-sleuth" },
-	{
-		"Gnvimtools/none-ls.nvim",
-		config = function()
-			local null_ls = require("null-ls")
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.prettier,
-				},
-				on_attach = function(client, bufnr)
-					-- Enable formatting on sync
-					if client.supports_method("textDocument/formatting") then
-						local format_on_save = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
-						vim.api.nvim_create_autocmd("BufWritePre", {
-							group = format_on_save,
-							buffer = bufnr,
-							callback = function()
-								vim.lsp.buf.format({
-									bufnr = bufnr,
-									filter = function(_client)
-										return _client.name == "null-ls"
-									end,
-								})
-							end,
-						})
-					end
-				end,
-			})
-		end,
-	},
 }
