7426894ebb19d865fb97a5d02f900efa7d13952d
Tamas Csontos <csontos.t77@gmail.com>
2024-06-13 00:22:27 +0200

    nvim: create switchable neovim configs

---
+++ LazyVim/lua/plugins/theme.lua
@@ -0,0 +1,11 @@
+return {
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme("catppuccin")
+    vim.cmd(":highlight LineNr guifg=#585b70")
+    vim.cmd(":highlight WinSeparator guifg=#313244")
+  end,
+}
