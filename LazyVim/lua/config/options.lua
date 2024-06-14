955db5775c18684bfdb5afd6bc8cd6504d989f68
Tamas Csontos <csontos.t77@gmail.com>
2024-06-14 15:40:09 +0200

    nvim: add line moving keymaps

--- LazyVim/lua/config/options.lua
+++ LazyVim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Move lines in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
